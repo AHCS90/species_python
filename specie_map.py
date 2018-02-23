d27b2c56874c5828e84685d54f59fb5420ac3ea5
AHCS90 <35605685+AHCS90@users.noreply.github.com>
2018-02-23 12:59:13 +0100

    prueba + vuelta a versión anterior

--- specie_map.py
+++ specie_map.py
@@ -14,23 +14,22 @@ specie2 = pd.read_csv('S_pilchaldrus_record.csv')
 lon, lat = specie['decimalLongitude'], specie['decimalLatitude']
 lon1, lat1 = specie1['decimalLongitude'], specie1['decimalLatitude']
 lon2, lat2 = specie2['decimalLongitude'], specie2['decimalLatitude']
-# MODIFICABLE
-# Lectura de datos adicionales (se deben convertir a cadena para visualizarlos)
+
+
+#AÑADIENDO LA FECHA
+# Extraemos las fechas del conjunto de datos, los cuales utilizaremos para la creación de marcadores.
 dates = specie['eventDate'].astype('str')
 dates1 = specie1['eventDate'].astype('str')
 dates2 = specie2['eventDate'].astype('str')
 
-# MODIFICABLE
-# Opciones de visualizacion de la especie
-# Debeis ajustar las coordenadas y el zoom del mapa a la localizacion de la especie
-# Muchas mas en: http://python-visualization.github.io/folium/docs-v0.5.0/modules.html
+# CREACIÓN DEL MAPA PARA VISUALIZACION
 m = folium.Map(location=[50, 10], zoom_start=3, tiles='Stamen Watercolor')
 
 # Creacion del conjunto de puntos
 feature_group = folium.FeatureGroup('Ocurrences')
 
 
-# MODIFICABLE
+# 
 for lon, lat, dates in zip(lon, lat, dates):
     feature_group.add_child(folium.RegularPolygonMarker(location=[lat, lon], popup = dates, color='#af1818', fill_color='#af1818', number_of_sides=4, radius=3))
 
